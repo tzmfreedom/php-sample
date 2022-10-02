af79a8dae84f2387ab7d4316ab14136ab0b4494f
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2022-10-02 09:04:10 +0900

    fix

--- panther/index.php
+++ panther/index.php
@@ -8,13 +8,19 @@ $client = Client::createChromeClient();
 // Or, if you care about the open web and prefer to use Firefox
 // $client = Client::createFirefoxClient();
 
+$client->request('GET', 'http://localhost:8080/web.php');
+$crawler = $client->waitFor('#main');
+echo $crawler->filter('#main')->text() .PHP_EOL;
+exit;
+
 $client->request('GET', 'https://api-platform.com'); // Yes, this website is 100% written in JavaScript
 $client->clickLink('Get started');
+
 
 // Wait for an element to be present in the DOM (even if hidden)
 $crawler = $client->waitFor('#installing-the-framework');
 // Alternatively, wait for an element to be visible
 $crawler = $client->waitForVisibility('#installing-the-framework');
 
-echo $crawler->filter('#installing-the-framework')->text();
+echo $crawler->filter('#installing-the-framework')->text() .PHP_EOL;
 $client->takeScreenshot('screen.png'); // Yeah, screenshot!
---
+++ panther/web.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<script>
+window.onload = function() {
+  document.getElementById('main').textContent = 123;
+};
+</script>
+</head>
+<body>
+<div id="main"></div>
+</body>
+</html>
